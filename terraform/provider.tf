dc74df738a1b79b42fc99e5f286f2b58a49e3bca
Karthick-25-11 <karthick.ceg20@gmail.com>
2026-05-12 14:30:39 +0000

    chore: finalize S3 backend migration and untrack local state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+#Remote state locking
+terraform {
+  backend "s3" {
+    bucket         = "gitops-state-bucket"
+    key            = "gitops-mvp/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
